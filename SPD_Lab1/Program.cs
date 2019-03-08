21aeb5b6190a90acb77d300462e26657cead627a
Matt <mateo.rook@gmail.com>
2019-03-08 21:58:43 +0100

    coś tam

--- SPD_Lab1/Program.cs
+++ SPD_Lab1/Program.cs
@@ -11,56 +11,48 @@ namespace SPD_Lab1
     {
         static void Main(string[] args)
         {
-            //    int iloscZadan,iloscMaszyn;
-            //    string line;
-            //    Zadanie[] zadania;
+            int iloscZadan, iloscMaszyn;
+            string line;
+            Zadanie[] zadania;
+            int minSpan;
 
-            //    try
-            //    {
-            //        Console.Write("PathToFile: ");
-            //        string path = Console.ReadLine();
-
-            //        using (StreamReader sr = new StreamReader(path))
-            //        {
-
-            //            line = sr.ReadLine();
-            //            string[] znaki = line.Split(new char[] {' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
-
-            //            iloscZadan = int.Parse(znaki[0]);
-            //            iloscMaszyn = int.Parse(znaki[1]);
-
-            //            zadania = new Zadanie[iloscZadan];
-
-            //            for (int i = 0; i < iloscZadan; i++)
-            //            {
-            //                line = sr.ReadLine();
-            //                znaki = line.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
-            //                int[] liczby = new int[znaki.Length];
-
-            //                for(int j = 0; j < znaki.Length; j++)
-            //                {
-            //                    liczby[j] = int.Parse(znaki[j]);
-            //                }
-            //                zadania[i] = new Zadanie(iloscMaszyn, liczby);
-            //            }
-            //        }
-            //        foreach (var z in zadania)
-            //        {
-            //            z.Wypisz();
-            //        }
-            //    }
-            //    catch (Exception e)
-            //    {
-            //        Console.Write(e.Message); Console.ReadKey();
-            //    }
-            int[] a = { 1, 2, 3};
-            foreach (var p in Util<int>.Permute(a))
+            try
             {
-                foreach(var x in p)
+                Console.Write("PathToFile: ");
+                string path = Console.ReadLine();
+
+                using (StreamReader sr = new StreamReader(path))
                 {
-                    Console.Write(x);
+
+                    line = sr.ReadLine();
+                    string[] znaki = line.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    iloscZadan = int.Parse(znaki[0]);
+                    iloscMaszyn = int.Parse(znaki[1]);
+
+                    zadania = new Zadanie[iloscZadan];
+
+                    for (int i = 0; i < iloscZadan; i++)
+                    {
+                        line = sr.ReadLine();
+                        znaki = line.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                        int[] liczby = new int[znaki.Length];
+
+                        for (int j = 0; j < znaki.Length; j++)
+                        {
+                            liczby[j] = int.Parse(znaki[j]);
+                        }
+                        zadania[i] = new Zadanie(iloscMaszyn, liczby);
+                    }
                 }
-                Console.WriteLine();
+                foreach (var p in Util<Zadanie>.Permute(zadania))
+                {
+                   // TODO: policz min C span
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message); Console.ReadKey();
             }
         }   
     }
