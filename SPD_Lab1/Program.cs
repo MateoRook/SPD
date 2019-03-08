3dd4203a8f62d7b211c141b5e7f52b0cd79d2a86
Matt <mateo.rook@gmail.com>
2019-03-08 21:34:52 +0100

    Dodano klasę Util do pomocniczych metod

--- SPD_Lab1/Program.cs
+++ SPD_Lab1/Program.cs
@@ -11,45 +11,57 @@ namespace SPD_Lab1
     {
         static void Main(string[] args)
         {
-            int iloscZadan,iloscMaszyn;
-            string line;
-            try
-            {
-                Console.Write("PathToFile: ");
-                string path = Console.ReadLine();
+            //    int iloscZadan,iloscMaszyn;
+            //    string line;
+            //    Zadanie[] zadania;
 
-                using (StreamReader sr = new StreamReader(path))
+            //    try
+            //    {
+            //        Console.Write("PathToFile: ");
+            //        string path = Console.ReadLine();
+
+            //        using (StreamReader sr = new StreamReader(path))
+            //        {
+
+            //            line = sr.ReadLine();
+            //            string[] znaki = line.Split(new char[] {' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+            //            iloscZadan = int.Parse(znaki[0]);
+            //            iloscMaszyn = int.Parse(znaki[1]);
+
+            //            zadania = new Zadanie[iloscZadan];
+
+            //            for (int i = 0; i < iloscZadan; i++)
+            //            {
+            //                line = sr.ReadLine();
+            //                znaki = line.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            //                int[] liczby = new int[znaki.Length];
+
+            //                for(int j = 0; j < znaki.Length; j++)
+            //                {
+            //                    liczby[j] = int.Parse(znaki[j]);
+            //                }
+            //                zadania[i] = new Zadanie(iloscMaszyn, liczby);
+            //            }
+            //        }
+            //        foreach (var z in zadania)
+            //        {
+            //            z.Wypisz();
+            //        }
+            //    }
+            //    catch (Exception e)
+            //    {
+            //        Console.Write(e.Message); Console.ReadKey();
+            //    }
+            int[] a = { 1, 2, 3};
+            foreach (var p in Util<int>.Permute(a))
+            {
+                foreach(var x in p)
                 {
-
-                    line = sr.ReadLine();
-                    string[] znaki = line.Split(new char[] {' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
-
-                    iloscZadan = int.Parse(znaki[0]);
-                    iloscMaszyn = int.Parse(znaki[1]);
-
-                    Zadanie[] zadania = new Zadanie[iloscZadan];
-                    for (int i = 0; i < iloscZadan; i++)
-                    {
-                        line = sr.ReadLine();
-                        znaki = line.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
-                        int[] liczby = new int[znaki.Length];
-
-                        for(int j = 0; j < znaki.Length; j++)
-                        {
-                            liczby[j] = int.Parse(znaki[j]);
-                        }
-                        zadania[i] = new Zadanie(iloscMaszyn, liczby);
-                    }
-                    foreach (var z in zadania)
-                    {
-                        z.Wypisz();
-                    }
+                    Console.Write(x);
                 }
+                Console.WriteLine();
             }
-            catch (Exception e)
-            {
-                Console.Write(e.Message); Console.ReadKey();
-            }
-        }
+        }   
     }
 }
---
+++ SPD_Lab1/Util.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPD_Lab1
+{
+    public static class Util<T>
+    {
+        public static IEnumerable<T[]> Permute(T[] zad, int low = 0)
+        {
+            T temp;
+            if (low + 1 >= zad.Length)
+                yield return zad;
+            else
+            {
+                foreach (var p in Permute(zad, low + 1))
+                    yield return p;
+
+                for (int i = low + 1; i < zad.Length; i++)
+                {
+                    temp = zad[low];
+                    zad[low] = zad[i];
+                    zad[i] = temp;
+
+                    foreach (var p in Permute(zad, low + 1))
+                        yield return p;
+
+                    temp = zad[low];
+                    zad[low] = zad[i];
+                    zad[i] = temp;
+                }
+            }
+        }
+    }
+}
